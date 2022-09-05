b3bc043b24149c225d600d77d5151c5ebe2c173a
mila <milavoronina1997@mail.ru>
2022-09-06 01:35:04 +0500

    Task5_1

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход целое положительное число N и координаты двух точек и находит
+// расстояние между ними в N-мерном пространстве.
+
+Console.WriteLine("Введите число для определения пространства: ");
+int N = Convert.ToInt32(Console.ReadLine);
+Console.WriteLine("Введите первую координату: ");
+int a = Convert.ToInt32(Console.ReadLine);
+Console.WriteLine("Введите вторую координату: ");
+int b = Convert.ToInt32(Console.ReadLine);
+Math.Sqrt()
+return Math.Sqrt(Math.Pow((xb-xa),2)+Math.Pow((yb-ya),2)+Math.Pow((zb - za),2));  
+
+{\displaystyle d(p,q)={\sqrt {(p_{1}-q_{1})^{2}+( p_{2}-q_{2})^{2}+\ cdots +(p_{i}-q_{i})^{2}+\cdots +(p_{n}-q_{n})^{2}}}.}
+
